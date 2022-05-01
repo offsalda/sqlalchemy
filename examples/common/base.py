e343f887b881281c2dde9f1b346d36183998cf65
Antonio <adias@atech.com.br>
2022-05-01 15:58:08 -0300

    teste

--- examples/common/base.py
+++ examples/common/base.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('postgresql://dbuser:dbpassword@localhost:5432/sqlalchemy-orm-tutorial')
+#engine = create_engine('postgresql://dbuser:dbpassword@localhost:5432/sqlalchemy-orm-tutorial')
+engine = create_engine('mysql://root:mudar123@localhost:3306/sqlalchemy-orm-tutorial')
+
 # use session_factory() to get a new Session
 _SessionFactory = sessionmaker(bind=engine)
 
